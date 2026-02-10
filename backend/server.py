f57aaf30a6de32a85f0b7e6fa0f844f7e1302126
Shaik Asif <156982927+shaik-asif0@users.noreply.github.com>
2026-02-10 16:09:18 +0530

    update server

--- backend/server.py
+++ backend/server.py
@@ -33,15 +33,15 @@ import shutil
 from openai import AzureOpenAI, OpenAI
 
 
-# from fastapi.middleware.cors import CORSMiddleware
-# app = FastAPI()
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],  # allow all for now
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+from fastapi.middleware.cors import CORSMiddleware
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all for now
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
